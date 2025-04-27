f9bc0a11dab489e7319f09a331af183e5913fc5f
abanoubmamdou7 <abanoub.mamdouh@smartupeg.com>
2025-04-27 16:53:42 +0300

    first commit

---
+++ src/modules/app.router.js
@@ -0,0 +1,32 @@
+import cookieParser from "cookie-parser";
+import connectDB from "../../DB/connection.js";
+import { glopalErrHandling } from "../utils/errorHandling.js";
+import { AppError } from "../utils/appError.js";
+const initApp = (app, express) => {
+  // Built-in Middleware
+  app.use(express.json());
+  app.use(cookieParser());
+
+  // Custom Middlewares
+
+  // Routes
+
+
+  // Catch-all for undefined routes
+  app.use((req, res, next) => {
+    next(
+      new AppError("Not Found", 404, {
+        method: req.method,
+        url: req.originalUrl,
+      })
+    );
+  });
+
+  // Global Error Handler
+  app.use(glopalErrHandling);
+
+  // Connect to DB
+  connectDB();
+};
+
+export default initApp;
